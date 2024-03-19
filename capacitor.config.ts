d8be772b058b604c88a140575c0cf68a2d5d38ae
Ken Singson <kontzeen@gmail.com>
2024-03-19 22:54:41 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Virtuoso_Labs',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
