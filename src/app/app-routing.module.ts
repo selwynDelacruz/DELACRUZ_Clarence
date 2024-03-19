8c18c7842c84c3b4b11a0467673c784defb82aef
Ken Singson <kontzeen@gmail.com>
2024-03-19 23:03:04 +0800

    ACT1-GROUPCOMMIT1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'assignment1',
+    loadChildren: () => import('./assignment1/assignment1.module').then (m => m.assignment1) // assignment 1 path
+  },
+  {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
---
+++ src/app/assignment1/our-custom-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: 'assignmentpage.html',
+})
+export class HomePage {
+  groupName = ' I am Ken Singson, I am from Group Virtuoso Labs!'; // add GroupName
+  constructor() {}
+}
