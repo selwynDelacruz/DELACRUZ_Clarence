3551646ac6bbd16328e61e1ddfaa4eb88b55f535
SeseGuru <134850743+d4rky19@users.noreply.github.com>
2024-03-27 09:48:21 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Activity5-DELACRUZ',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
